7ef0cdb296e6e8da672b6a6c9cb975166f001b0d
marcellogomes-santos <marcello.gomes@aluno.ifsp.edu.br>
2026-08-03 17:42:16 -0300

    modificação na interface

--- App.js
+++ App.js
@@ -1,34 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Portanto, irmãos, peço, pelas misericórdia de Deus, que ofereçam o corpo de vocês como sacrifício vivo, santo e agradável a Deus: este é o culto racional de vocês.!</Text>
-      <Text>Romanos 12:1</Text>
-      <Text>Versículo do Dia </Text>
-      <View style={styles.box}></View>
+      <View style={styles.box}>
+        <Image style={styles.logo}
+          source={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSubR20yAwHLOdRMuvx9IgNQ6DUwIWw1c5pa_SJc1e4tA&s'} />
+        <Text>La Bolaria</Text>
+      </View>
+      <View style={styles.box2}>
+        <View style={styles.card}>
+
+          <Image style={styles.bolo} source={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6aICLyTz5BQwx6l9knyS24JZXX0PVSNC51DT6sRbtsg&s=10'} />
+          <Text style={styles.text}>Bolo Fit de Maçã</Text>
+        </View>
+        <View style={styles.card}>
+
+          <Image style={styles.bolo} source={'https://labolaria.com.br/wp-content/uploads/2025/10/16-bolo-caribe-200x300.webp'} />
+          <Text style={styles.text}>Bolo Caribe</Text>
+
+        </View>
+        <View style={styles.card}>
+
+          <Image style={styles.bolo} source={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNaIdm02Z_Ja-iBcOnBsP5zqDgfgn75zjS0Kr28ZBBVINZKVaknRns2Vg&s=10'} />
+          <Text style={styles.text}>Bolo Ferrero Rocher</Text>
+
+        </View>
+      </View>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Ocupa toda a tela
+    backgroundColor: '#6670f5',
+
   },
-  titulo: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#09641c',
-  },
+
   box: {
+    flex: 2,
+    backgroundColor: "#f9e895",
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20
+  },
+
+  box2: {
+    flex: 5,
+    backgroundColor: 'green',
+
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 20
+  },
+
+  card: {
+    flexDirection: 'row',
+    margin: 10,
+    borderColor: 'black',
+    borderWidth: 5
+
+  },
+
+  bolo: {
     width: 200,
     height: 200,
-    backgroundColor: '#224b2a',
-   
+    boderRadius: 25,
+    margin: 10,
+
   },
+
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+
+
+  },
+
 });
--- README.md
+++ README.md
@@ -12,16 +12,42 @@ Linguagem de programação
 JavaScript é uma linguagem de programação usada para criar páginas e aplicações interativas.
 
 O que é o Node.js?
-R: Node.js é um ambiente que permite executar JavaScript fora do navegador, como em servidores.
+R: Node.js é um ambiente que permite executar JavaScript fora do navegador, como em servidores.(Run Time é a mesma coisa de ambiente de execução)
 
-O que é a biblioteca React? 
+O que é a biblioteca React? (core - coração, núcleo, base)
 R: React é uma biblioteca JavaScript usada para criar interfaces de usuário de forma rápida e organizada.
 
 o que é biblioteca React Native?
-R: É uma biblioteca que fornece os componentes necessários para desenvolver aplicativos para dispositivos moveis.
+R: É uma biblioteca que fornece os componentes necessários para desenvolver aplicativos para dispositivos moveis. (Componentes nativos).
 
 O que é a Framework Expo?
-R: Expo é um framework que facilita o desenvolvimento de aplicativos móveis com React Native.
+R: Expo é um framework que facilita o desenvolvimento de aplicativos móveis com React Native. Framework fornece um ecossistema para facilitar nosso desenvolvimento.
 
-Expo --> é um framework que usa como base react native
+Expo --> é um framework que usa como base react native. (usa react native)
+    -->  
+React Native --> é uma biblioteca que usa como base o react. (usa react)
+
+React --> é uma biblioteca JavaScript.
+
+O que é npm (NODE PACKAGE MANAGER)?
+R: É um gerenciador de pacotes do Node.( Permite instalar, desintalar, atualizar e executar escripts) (Pacote de códigos que serão usados no projeto). Ex: A camera do celular, o GPS, oAccelerometer, etc.
+
+Comando de instalação do Expo:
+npx create-expo-app@lastest --template
+
+Opções:
+- Blank
+- nome do App
+- For learning with Expo Go (SDK 54 ) ** mais estável do q a versão 57
+
+comando paea rodar o app:
+npm start ou npm run web
+
+O StyleSheet do React Natiove tem todas as propriedades da Web? (CSS)
+R: Não. Mas possui as principais, já que o foco é mobile.
+Não. O StyleSheet do React Native possui muitas propriedades semelhantes ao CSS, mas não todas. Algumas propriedades da Web não são suportadas.
+
+Todos as propriedades do StyleSheet funcionam para IOs e Android?
+R: Não. Algumas propriedades são específicas para IOS e ouutras para Android, mas a maioria funciona para os dois sistemas.
+Não. A maioria funciona nos dois sistemas, mas algumas propriedades são exclusivas ou se comportam de forma diferente no iOS e no Android.
 
